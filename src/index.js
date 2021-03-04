b6006a12463cb1e8d52d1398e7e40a051525efec
bKedds <benkeddie@gmail.com>
2021-03-04 10:34:10 -0800

    added redux

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './containers/App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { applyMiddleware, createStore, combineReducers } from "redux";
+import { searchRobots, requestRobots } from "./reducers";
+import { createLogger } from 'redux-logger';
+import thunkMiddleware from 'redux-thunk';
+import "./index.css";
+import App from "./containers/App";
+import * as serviceWorker from "./serviceWorker";
 import "tachyons";
 
+const logger = createLogger();
+const rootReducer = combineReducers({searchRobots, requestRobots})
+const store = createStore(rootReducer, applyMiddleware(thunkMiddleware, logger));
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.getElementById("root")
+);
 
 serviceWorker.unregister();
